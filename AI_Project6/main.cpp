3ae0112819f26f096ae58405b2fc816a8daec18c
Henry West <henrywest93@outlook.com>
2014-12-02 21:36:57 -0600

    Initial Commit

---
+++ AI_Project6/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AI_Project6
+//
+//  Created by Henry West on 12/2/14.
+//  Copyright (c) 2014 Henry West. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
